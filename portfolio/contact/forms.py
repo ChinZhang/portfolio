562e3db073062512652bfa72cb2ec2bf3e879b15
ChinZhang <chinzh00@gmail.com>
2021-02-18 15:18:39 -0500

    Formatted contact me section

--- portfolio/contact/forms.py
+++ portfolio/contact/forms.py
@@ -2,7 +2,27 @@ from django import forms
 
 
 class ContactForm(forms.Form):
-    name = forms.CharField(label="Name", max_length=130, required=True)
-    subject = forms.CharField(label="Subject", max_length=130, required=True)
-    email = forms.EmailField(label="Email", max_length=130, required=True)
-    message = forms.CharField(label="Message", max_length=2000, required=True, widget=forms.Textarea)
+    name = forms.CharField(
+        label='',
+        max_length=130,
+        required=True,
+        widget=forms.TextInput(attrs={'placeholder': 'Name'})
+    )
+    subject = forms.CharField(
+        label='',
+        max_length=130,
+        required=True,
+        widget=forms.TextInput(attrs={'placeholder': 'Subject'})
+    )
+    email = forms.EmailField(
+        label="",
+        max_length=130,
+        required=True,
+        widget=forms.TextInput(attrs={'placeholder': 'Email'})
+    )
+    message = forms.CharField(
+        label="",
+        max_length=2000,
+        required=True,
+        widget=forms.Textarea(attrs={'placeholder': 'Message'})
+    )
