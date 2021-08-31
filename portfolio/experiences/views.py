c9b53abdfb7b3adc9176b84b1918824cef3e253e
ChinZhang <chinzh00@gmail.com>
2021-08-31 11:00:48 -0400

    Set up skeletion for project section and cleaned up project

--- portfolio/experiences/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .models import Experience
-
-
-# Create your views here.
-def experience_page(request):
-    experiences = Experience.objects.order_by('start_date')
-    return render(request, '../templates/experiences_page.html', {'experiences': experiences})
