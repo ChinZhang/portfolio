f59a054095bc510765beca514b944aefaa4a8ebb
ChinZhang <chinzh00@gmail.com>
2021-09-16 23:03:13 -0400

    Adjusted project to allow pull in images from database

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -130,7 +130,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static/resources/project_images/'),
 ]
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
